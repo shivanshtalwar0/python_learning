21b74a124672a4b9d8d0421c35a79c986e6838b2
shivanshtalwar <shivansh@utopia.tech>
2024-02-20 19:35:08 +0200

    first

---
+++ sunidhi.py
@@ -0,0 +1,55 @@
+#print('{name} is a good girl studying {language}'.format(name='sunidhi',language='python'))
+#print('pehli ' +   '\tdoosri'     )
+
+# 0=>1 1=>2 2=>4
+# format(...args)=> multiple arguments 
+#    0,1,2,3
+# fn(1,2,4,5)=> 
+# computer's memory 0 index se start hoti hai in python, java ,c++
+
+# concatenation => joining two things together
+# two strings together concatenation operator +
+# group of one or many characters represented in python by ('') or double quotes ("")
+# multi-line string '''hh''' """kk"""
+# (\) \n, \t 
+# \n => new line escape character
+# variableName=78888
+
+#Boolean => true/ /0/1
+#yes(true) /no(false)
+# 50paise toffe khalio "kissMe"
+# 0.50Rs 7776767676.767666767
+# -7778787 to 67868686686, -infinity to + infinity including 0
+
+# fall inside black hole ? bachega ya marega 
+# None 
+
+# BODMAS
+# () > */ > +- > or and
+# baarish hogi and chai banegi => pakode pakka banege
+# baarish hogi or chai banegi => pakode pakka banege
+
+#str(9)# => '9'
+# negative indexing is not allowed in many languages
+# python:i support negative indexing
+
+sunidhi='suny'
+print(sunidhi[0])
+print(sunidhi[1])
+print(sunidhi[2])
+print(sunidhi[3])
+print('ulta dikhega')
+print(sunidhi[-1])
+print(sunidhi[-2])
+print(sunidhi[-3])
+print(sunidhi[-4])
+
+# slicing is a way to cut or extract a part of a string
+# sunidhi[0:16] => 'i am a good girl'
+# sunidhi[16:] => ', studying python'
+#=>
+sunidhi='hello'
+print(sunidhi[0:3])
+print(sunidhi[3:])
+print(sunidhi[-4:])
+
