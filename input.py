085c47ed9a4c3382c7f6cecd594c7c2e7d6ea5ff
shivanshtalwar <shivansh@utopia.tech>
2024-02-20 19:49:20 +0200

    challenge

---
+++ input.py
@@ -0,0 +1,11 @@
+# taking input from HOOman
+# for this we have something 
+name=input('what is your name?')
+age=input('what is your age?')
+print('Your name is {name} and your age is {age}'.format(name=name,age=age))
+
+# You have an exercise in which you have to make a simple addition calculator
+# what is first number?:
+# what is second number?:
+# sum is {sum}
+# hint:- by default input gives you a string and you need to add the two numbers from input
